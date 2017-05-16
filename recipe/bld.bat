fbc9994b69ea1e37727095021eb2b75cbdab7a7c
Danilo Horta <danilo.horta@gmail.com>
2017-05-16 15:24:42 +0100

    Copy dll

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,4 +4,7 @@ set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 
 "%PYTHON%" setup.py build_ext -I"%LIBRARY_INC%" -lliknorm -L"%LIBRARY_LIB%" ^
            install --single-version-externally-managed --record record.txt
+
+copy %PREFIX%\Lib\liknorm.dll %LIBRARY_BIN%
+
 if errorlevel 1 exit 1
