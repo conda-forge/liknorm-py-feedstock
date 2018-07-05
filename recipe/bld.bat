0f5fbcc562aae329c544ca5a760b88701847c0b5
Danilo Horta <danilo.horta@gmail.com>
2018-07-05 21:08:47 +0100

    install

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,16 +2,9 @@ set LIB=%LIBRARY_LIB%;.\lib;%LIB%
 set LIBPATH=%LIBRARY_LIB%;.\lib;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 
-IF "%PY_VER%"=="2.7" (
-    set LLIB=-lliknorm -lhcephes
-    copy %PREFIX%\Lib\hcephes.dll %LIBRARY_BIN%
-) ELSE (
-    set LLIB=-lliknorm
-)
+set LLIB=-lliknorm
 
 %PYTHON% setup.py build_ext -I"%LIBRARY_INC%" %LLIB% -L"%LIBRARY_LIB%" ^
            install --single-version-externally-managed --record record.txt
-
-copy %PREFIX%\Lib\liknorm.dll %LIBRARY_BIN%
 
 if errorlevel 1 exit 1
