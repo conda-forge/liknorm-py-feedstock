341cc06d20f795617f9b7a301dd67537ff92502e
Danilo Horta <danilo.horta@gmail.com>
2018-07-06 14:41:47 +0100

    simplyfied bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,4 @@
-set LIB=%LIBRARY_LIB%;.\lib;%LIB%
-set LIBPATH=%LIBRARY_LIB%;.\lib;%LIBPATH%
-set INCLUDE=%LIBRARY_INC%;%INCLUDE%
-
-set LLIB=-lliknorm
-
-%PYTHON% setup.py build_ext -I"%LIBRARY_INC%" %LLIB% -L"%LIBRARY_LIB%" ^
+%PYTHON% setup.py build_ext -I"%LIBRARY_INC%" -lliknorm -L"%LIBRARY_LIB%" ^
            install --single-version-externally-managed --record record.txt
 
 if errorlevel 1 exit 1
